5948d5865e0d8b395e8fcc6750e940d7f550fa28
vickykumar62 <vickykumar1182005@gmail.com>
2026-03-07 09:41:06 +0530

    how to pass decresing parameter

---
+++ recursion/basics/03_print_number_from_n_to_1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int n){
+    if(n<0) return;
+    cout<<n<<endl;
+    solve(n-1);
+}
+
+int main(){
+    int n = 10;
+    solve(n);
+    return 0;
+}
