2a7c4d95709e59c08538d5a53944bca00478edc1
vickykumar62 <vickykumar1182005@gmail.com>
2026-03-07 18:27:24 +0530

    solve without using the extra space

--- recursion/grid/02_word_search.cpp
+++ recursion/grid/02_word_search.cpp
@@ -73,3 +73,42 @@ public:
         return false;
     }
 };
+
+// solved without using extra space 
+// for visited board[i][j] = '#' and while returning assign it with it's original previous val
+
+class Solution {
+public:
+    bool solve(int idx , int i , int j , 
+    string word , vector<vector<char>>& board){
+        if(idx==word.length()) return true;
+        if(i<0 || j<0 || i>=board.size() || j>= board[0].size() || board[i][j]=='#') return false;
+        if(word[idx] != board[i][j]) return false;
+        
+
+        char c = board[i][j];
+        board[i][j] = '#';
+
+       if( solve(idx+1,i+1,j,word,board) ) return true; // d 
+       if( solve(idx+1,i,j-1,word,board) ) return true; // l
+       if( solve(idx+1,i,j+1,word,board) ) return true; // r
+       if( solve(idx+1,i-1,j,word,board) ) return true; //u
+       
+        board[i][j] = c;
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int row = board.size();
+        int col = board[0].size();
+
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(board[i][j]==word[0]){
+                    if (solve(0,i,j,word,board) ) return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
